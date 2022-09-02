84f4acfe954450d6f808f28e07d77586a7eb2560
Munde <17663747496@163.com>
2022-09-02 17:19:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Munde
+ * @create 2022-09-02 17:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
